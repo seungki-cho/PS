8d7f0898aea4f21e464d28946a08b330b30b101e
조승기 <57134892+seungki-cho@users.noreply.github.com>
2022-12-30 15:29:13 +0900

    [Silver II] Title: 베르트랑 공준, Time: 28 ms, Memory: 79752 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.swift
@@ -0,0 +1,79 @@
+import Foundation
+
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+
+var fio = FileIO()
+
+var isPrime = Array(repeating: true, count: (123456*2)+1)
+(isPrime[0], isPrime[1]) = (false, false)
+for i in 2..<isPrime.count {
+    if isPrime[i], i+i <= isPrime.count-1 {
+        for j in stride(from: i+i, through: isPrime.count-1, by: i) {
+            isPrime[j] = false
+        }
+    }
+}
+
+while true {
+    let n = fio.readInt()
+    if n == 0 { break }
+    print(isPrime[n+1...2*n].filter { $0 }.count)
+}
