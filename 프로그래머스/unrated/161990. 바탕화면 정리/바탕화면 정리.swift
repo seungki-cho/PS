a9a07029918cf803d1dade874754f008bb45d943
조승기 <57134892+seungki-cho@users.noreply.github.com>
2023-03-07 14:11:07 +0900

    [unrated] Title: 바탕화면 정리, Time: 1.01 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.swift
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.swift
@@ -1,5 +1,17 @@
 import Foundation
 
 func solution(_ wallpaper:[String]) -> [Int] {
-    return []
+    var arr = wallpaper.map{ Array($0).map { String($0) }}
+    var (a, b, c, d) = (Int.max, Int.max, -1, -1)
+    
+    for i in 0..<arr.count {
+        for j in 0..<arr[0].count where arr[i][j] == "#" {
+            a = [a, i].min()!
+            b = [b, j].min()!
+            c = [c, i+1].max()!
+            d = [d, j+1].max()!
+        }
+    }
+    
+    return [a, b, c, d]
 }
