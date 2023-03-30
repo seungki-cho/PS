f5905c0b88a5f3718ebd19ba16326e53f942af1b
조승기 <57134892+seungki-cho@users.noreply.github.com>
2023-03-30 21:49:50 +0900

    [unrated] Title: 추억 점수, Time: 0.09 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.swift
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.swift
@@ -1,5 +1,6 @@
 import Foundation
-
 func solution(_ name:[String], _ yearning:[Int], _ photo:[[String]]) -> [Int] {
-    return []
+    var dic = [String: Int]()
+    (0..<name.count).forEach { dic[name[$0]] = yearning[$0]}
+    return photo.map { $0.reduce(0, { $0 + (dic[$1] ?? 0)}) }
 }
