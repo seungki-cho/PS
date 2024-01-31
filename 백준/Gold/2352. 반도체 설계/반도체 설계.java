818507740a5a1c06d77267c7ee92b03dde37bc1d
조승기 <57134892+seungki-cho@users.noreply.github.com>
2024-01-31 11:27:55 +0900

    [Gold II] Title: 반도체 설계, Time: 228 ms, Memory: 19828 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int bs(ArrayList<Integer> li, int l, int r, int target) {
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (target > li.get(m)) {
+                l = m+1;
+            } else {
+                r = m;
+            }
+        }
+
+        return l;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] li = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            li[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        if (N == 1) {
+            System.out.println(1);
+            return;
+        }
+        ArrayList<Integer> dp = new ArrayList<>();
+        dp.add(li[0]);
+        for(int i = 1; i < N; i++) {
+            if (dp.get(dp.size()-1) < li[i]) {
+                dp.add(li[i]);
+                continue;
+            }
+            int idx = bs(dp, 0, dp.size()-1, li[i]);
+            dp.set(idx, li[i]);
+        }
+        System.out.println(dp.size());
+    }
+}
+
+/*
+
+4
+2_1_3_3
+5_1_2_1
+2_1_2_3
+5_1_1_1
+
+3
+7 4 5
+7 3 5
+4 6 6
+ */
