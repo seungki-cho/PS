831329b861ad4e3d85b7f00d89f42bc946969a1f
조승기 <57134892+seungki-cho@users.noreply.github.com>
2024-01-31 11:49:56 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 80 ms, Memory: 11996 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -4,30 +4,30 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+// 메모리: 13028 KB, 시간: 80 ms
 public class Main {
     static int N;
     static ArrayList<Integer> arr;
     static StringBuilder sb;
     static void find(int n) {
-        String dash = "";
-        for(int i = 0; i < n; i++) {
-            dash += "____";
+        StringBuilder dash = new StringBuilder();
+        for(int i = 0; i < n; i++) { // depth에 맞춘 _ 개수 상수 추가
+            dash.append("____");
         }
 
-        if (n == N) {
+        if (n == N) { // 종료조건
             sb.append(dash + "\"재귀함수가 뭔가요?\"\n");
             sb.append(dash + "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
             sb.append(dash + "라고 답변하였지.\n");
             return;
         }
 
-
         sb.append(dash + "\"재귀함수가 뭔가요?\"\n");
         sb.append(dash + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
         sb.append(dash + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
         sb.append(dash + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
 
-        find(n+1);
+        find(n+1); // 재귀
         sb.append(dash + "라고 답변하였지.\n");
     }
     public static void main(String[] args) throws IOException {
