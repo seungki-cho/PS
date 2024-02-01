49a226ce23eb204bd5e6aea584a8afbb58d2c50c
조승기 <57134892+seungki-cho@users.noreply.github.com>
2024-02-01 15:47:15 +0900

    [Gold V] Title: 신기한 소수, Time: 972 ms, Memory: 18472 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,32 +9,23 @@ public class Main {
     static ArrayList<Integer> li = new ArrayList<>();
     static boolean isP(int n) {
         for(int i = 2; i < n; i++) {
-            if ((n%i) == 0) {
-                return false;
-            }
+            if ((n%i) == 0) { return false; }
         }
         return true;
     }
     static void find() {
         int a = 0;
-        if (li.size() != 0){
+        if (!li.isEmpty()){
             a = li.stream().reduce((a1,b1) -> a1*10 + b1).get();
-            if (!isP(a)) {
-                return;
-            }
-        }
-        if (a == 25) {
-            System.out.println("HI");
+            if (!isP(a)) { return; }
         }
         if (li.size() == N) {
-            for(int i = 0; i < N; i++) {
-                System.out.print(li.get(i));
-            }
-            System.out.println();
+            System.out.println(a);
             return;
         }
         for(int i = 1; i < 10; i++) {
             if (li.size() == 0 && 1 == i ) { continue; }
+
             li.add(i);
             find();
             li.remove(li.size()-1);
@@ -50,16 +38,3 @@ public class Main {
         find();
     }
 }
-/*
-
-4 3
-1 2 3 4
-2 3 4 5
-3 4 5 6
-4 5 6 7
-2 2 3 4
-3 4 3 4
-1 1 4 4
-
-
- */
