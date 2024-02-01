30f20b0a010d6d7d8122e19570caad586c1cf3cb
조승기 <57134892+seungki-cho@users.noreply.github.com>
2024-02-01 15:32:51 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int ans = 21412419;
+        int[] sins = new int[N];
+        int[] sseuns = new int[N];
+
+        for(int i = 0; i < N ;i++ ){
+            st = new StringTokenizer(br.readLine());
+            sins[i] = Integer.parseInt(st.nextToken());
+            sseuns[i] = Integer.parseInt(st.nextToken());
+        }
+        if (N == 1) {
+            System.out.println(Math.abs(sins[0] - sseuns[0]));
+            return;
+        }
+        for(int i = 1; i < (1<<N); i++) {
+
+            int sin = 1;
+            int sseun = 0;
+            for(int j = 0; j < N; j++) {
+                if ((i & (1<<j)) != 0) {
+                    sin *= sins[j];
+                    sseun += sseuns[j];
+                }
+            }
+            ans = Math.min(Math.abs(sin - sseun), ans);
+        }
+        System.out.println(ans);
+    }
+}
+/*
+
+4 3
+1 2 3 4
+2 3 4 5
+3 4 5 6
+4 5 6 7
+2 2 3 4
+3 4 3 4
+1 1 4 4
+
+
+ */
