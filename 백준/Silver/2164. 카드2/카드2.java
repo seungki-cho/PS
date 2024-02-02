3ce1c344c2e6351b3475758ee2797513862e5443
조승기 <57134892+seungki-cho@users.noreply.github.com>
2024-02-02 16:52:22 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 45596 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -2,20 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
+    static class Queue<T> {
+        private LinkedList<T> q = new LinkedList<>();
 
+        int size() {
+            return q.size();
+        }
+        void enqueue(T t) {
+            q.addLast(t);
+        }
+        T dequeue() {
+            return q.removeFirst();
+        }
+    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        LinkedList<Integer> li = new LinkedList<>();
+        int N = Integer.parseInt(st.nextToken());
+        Queue<Integer> q = new Queue<>();
+
         for (int i = 1; i <= N; i++) {
-            li.add(i);
+            q.enqueue(i);
         }
-        while (li.size() != 1) {
-            li.removeFirst();
-            li.addLast(li.removeFirst());
+        while (q.size() != 1) {
+            q.dequeue();
+            q.enqueue(q.dequeue());
         }
-        System.out.println(li.get(0));
+        System.out.println(q.dequeue());
     }
 }
+// asdfasdfasdasdfasdfasd
